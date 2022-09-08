abc5cba5a057f6a9900fac3004ffb16b03306f18
Othino <micahothino44@gmail.com>
2022-09-08 13:52:38 -0700

    File that passes betty style

---
+++ bettyfile.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character.
+ * @c: Input character
+ * Return: 1 if lowercase, 0 if uppercase
+ */
+int _islower(int c)
+{
+if (c > 96 && c < 123)
+
+return (1);
+else
+
+return (0);
+}
